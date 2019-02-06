ed559f7d16e3c8bff8a90ff10995e4b5c45aa722
Ayush-Jain2 <ayushjainrksh@gmail.com>
2019-02-06 16:56:04 +0530

    Server created at localhost:5000

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    PORT = 5000 || process.env.port;
+
+app.get("/", function(req, res){
+    res.send("Home page coming soon...");
+})
+
+app.listen(PORT, function(err){
+    if(err)
+        console.log(err);
+    else
+        console.log("Server started at PORT : "+PORT);
+})

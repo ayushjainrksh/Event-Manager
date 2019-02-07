60759afa2014ac16da0a47a7d6c706f677fe67d6
Ayush-Jain2 <ayushjainrksh@gmail.com>
2019-02-07 11:10:02 +0530

    mongoose and method-override added

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var express = require('express'),
     app = express(),
-    PORT = 5000 || process.env.port;
+    PORT = 5000 || process.env.port,
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    methodOverride = require('method-override');
 
 app.set("view engine","ejs");
 
 app.get("/", function(req, res){
     res.render("home");
-})
+});
+
+// app.get("/events", function(req, res){
+//     res.send("This will be event route");
+// });
 
 app.listen(PORT, function(err){
     if(err)
         console.log(err);
     else
         console.log("Server started at PORT : "+PORT);
-})
+});

7672ec33d5e500bee2e5056d6842c64f2c4ee392
Ayush-Jain2 <ayushjainrksh@gmail.com>
2019-02-07 11:13:16 +0530

    passport js added

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var express = require('express'),
     PORT = 5000 || process.env.port,
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    methodOverride = require('method-override');
-
+    methodOverride = require('method-override'),
+    
+var passport = require('passport'),
+    localStrategy = require('passport-local'),
+    passportLocalMongoose = require('passport-local-mongoose');
+    
 app.set("view engine","ejs");
 
 app.get("/", function(req, res){

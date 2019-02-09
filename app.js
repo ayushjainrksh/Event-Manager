a1af744e2accb611a40f7b16b210d2670b24dbe7
Ayush-Jain2 <ayushjainrksh@gmail.com>
2019-02-10 02:04:47 +0530

    Mailing functionality added

--- app.js
+++ app.js
@@ -113,6 +113,18 @@ app.post("/application", function(req, res){
         else
         {
             foundApp.save();
+
+            const sgMail = require('@sendgrid/mail');
+            sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+            const msg = {
+            to: 'ayushjainfff@gmail.com',
+            from: 'ayushjainrksh@gmail.com',
+            subject: 'Sending with SendGrid is Fun',
+            text: 'and easy to do anywhere, even with Node.js',
+            html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+            };
+            sgMail.send(msg);
+
             res.redirect("/");
         }
     });
